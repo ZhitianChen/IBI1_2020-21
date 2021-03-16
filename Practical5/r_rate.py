c8be68c21a031671fb65497b6cc9d255a7519e06
ZhitianChen <79952811+ZhitianChen@users.noreply.github.com>
2021-03-16 20:53:23 +0800

    Delete r_rate.py

--- Practical5/r_rate.py
+++
@@ -1,7 +0,0 @@
-n=84 #start with 84 people.
-r=1.4 #each infected individual will on average infect 1.4 individuals.
-g=0 #count the generations of infecting
-for g in range (6): #generate for 5 times
-        n=n*(r+1) #calculate the infected people
-        g=g+1 #the generation has increased 1 time
-print n #print out the final result
