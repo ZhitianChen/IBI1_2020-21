4123f8b75a8182bb9b849c3f4256af235c7dce8e
Zhitian <zhitian.20@intl.zju.edu.cn>
2021-03-16 21:06:36 +0800

    fibonacci.py

---
+++ Practical5/fibonacci.py
@@ -0,0 +1,22 @@
+a=1
+b=1
+c=a+b #three numbers is a group
+print a
+print b
+print c
+g=1 #count the round
+for g in range(1,4):
+        a=b+c
+	b=a+c
+	c=a+b #to add up
+        g=g+1 #the round number increases
+        print a
+        print b
+        print c
+d=b+c
+print d #the thirteenth number
+
+helloworld.py
+a="hello"
+b="wolrd!"
+print(a+b)
