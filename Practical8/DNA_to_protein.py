1037604525b5a4506a0b0c48cc872d3ffd707215
ZhitianChen <79952811+ZhitianChen@users.noreply.github.com>
2021-05-18 12:48:35 +0800

    Update DNA_to_protein.py

--- Practical8/DNA_to_protein.py
+++ Practical8/DNA_to_protein.py
@@ -1,1 +1,9 @@
-
+import re
+# to set up a dictionary including all the genetic code
+Dictionary = {'TTT': 'F', 'TTC': 'F', 'TTA': 'L', ' TTG': 'L', 'TCT': 'S', 'TCC': 'S', 'TCA': 'S', 'TCG': 'S', 'TAT': 'Y', 'TAC': 'Y', 'TAA': 'O', 'TAG': 'U', 'TGT': 'C', 'TGC': 'C', 'TGA': 'X', 'TGG': 'W', 'CTT': 'L', 'CTC': 'L', 'CTA': 'L', 'CTG': 'L', 'CCT': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P', 'CAT': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Z', 'CGT': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R', 'ATT': 'I', 'ATC': 'I', 'ATA': 'J', 'ATG': 'M', 'ACT': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': ' T', 'AAT': 'N', 'AAC': 'B', 'AAA': 'K', 'AAG': 'K', 'AGT': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R', 'GTT': 'V', 'GTC': 'V', 'GTA': 'V', 'GTG': 'V', 'GCT': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A', 'GAT': 'D', 'GAC': 'D', 'GAA': 'D', 'GAG': 'E', 'GGT': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G'}
+seq = 'ATGCGACTACGATCGAGGGCC'  # import the sequence
+groups = re.findall(r'[A-Z]{3}', seq)
+result = ''
+for i in range(0, len(groups)):
+    result = result + Dictionary[groups[i]]
+print(result)
